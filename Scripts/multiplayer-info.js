1e0471efea2557f1d7fa5a37b505fae977adab0b
Arjan Kwakkel <arjan@unfootprint.com>
2025-10-02 15:00:26 +0200

    Add icon to multiplayer info script

--- Scripts/multiplayer-info.js
+++ Scripts/multiplayer-info.js
@@ -6,6 +6,7 @@
 // @author       Natrium
 // @match        https://geo-fs.com/geofs.php*
 // @match        https://*.geo-fs.com/geofs.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
 // @grant        none
 // ==/UserScript==
 
