75876c76b1a6996d3d791c4425f5f4e6ffc60ea8
Arjan Kwakkel <arjan@unfootprint.com>
2025-10-12 20:32:16 +0200

    Add GEOFS Chat fix script

---
+++ Scripts/geo-fs-chat-fix.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         GeoFS Chat Fix
+// @namespace
+// @version      0.0.2
+// @description  Fixes the removal of the t keybind for opening the chat window in GeoFS.
+// @author       Zeta
+// @license
+// @match        https://www.geo-fs.com/geofs.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// ==/UserScript==
+
+(function () {
+    window.addEventListener("keyup", function (e) {
+        // Only trigger when no input field is focused
+        if (document.activeElement.tagName.toLowerCase() !== "input" &&
+            document.activeElement.tagName.toLowerCase() !== "textarea") {
+            // T key (keyCode 84)
+            if (e.keyCode === 84) {
+                e.stopPropagation();
+                ui.chat.showInput();
+            }
+        }
+    });
+})();
